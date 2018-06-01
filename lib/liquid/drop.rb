de84aba591c443fc86acf1982157527db2d5d442
Dmitry Borisov <scorpion061181@gmail.com>
2018-06-01 11:44:44 +0300

    по умолчанию дропы сериализуются в json как пустой хэш

--- lib/liquid/drop.rb
+++ lib/liquid/drop.rb
@@ -56,6 +56,10 @@ module Liquid
 
     alias_method :[], :invoke_drop
 
+    def to_json(options = {})
+      {}.to_json
+    end
+
     # Check for method existence without invoking respond_to?, which creates symbols
     def self.invokable?(method_name)
       invokable_methods.include?(method_name.to_s)
